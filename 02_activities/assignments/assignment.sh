5cc430f635a5949f8e7526ca14a2594c5357e9f7
azadeh <azadeh-selahvarzi@users.noreply.github.com>
2025-11-08 10:02:21 -0500

    question8

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -56,7 +56,10 @@ rm ./data/raw/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+touch ./data/inventory.txt 
+ls ./data/processed/server_logs >> ./data/inventory.txt 
+ls ./data/processed/user_logs >> ./data/inventory.txt 
+ls ./data/processed/event_logs >> ./data/inventory.txt 
 
 ###########################################
 
